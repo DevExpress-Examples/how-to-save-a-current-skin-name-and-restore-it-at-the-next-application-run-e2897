06dc82a03b0da0f7b8691f65bfa75e92d3b55f7b
DevExpressExampleBot <fakeEmail>
2026-02-06 17:50:53 +0400

    Source auto update [skip ci]

--- VB/WindowsApplication1/Form1.Designer.vb
+++ VB/WindowsApplication1/Form1.Designer.vb
@@ -19,7 +19,7 @@ Namespace WindowsApplication1
             MyBase.Dispose(disposing)
         End Sub
 
-'#Region "Windows Form Designer generated code"
+#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
@@ -69,7 +69,7 @@ Namespace WindowsApplication1
             Me.ResumeLayout(False)
         End Sub
 
-'#End Region
+#End Region
         Private galleryControl1 As DevExpress.XtraBars.Ribbon.GalleryControl
 
         Private galleryControlClient1 As DevExpress.XtraBars.Ribbon.GalleryControlClient
--- VB/WindowsApplication1/Form1.vb
+++ VB/WindowsApplication1/Form1.vb
@@ -14,12 +14,12 @@ Namespace WindowsApplication1
         Public Sub New()
             InitializeComponent()
             Call SkinHelper.InitSkinGallery(galleryControl1)
-            UserLookAndFeel.Default.SkinName = My.Settings.Default(CStr("ApplicationSkinName")).ToString()
+            UserLookAndFeel.Default.SkinName = Settings.Default(CStr("ApplicationSkinName")).ToString()
         End Sub
 
         Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As FormClosingEventArgs)
-            My.Settings.Default("ApplicationSkinName") = UserLookAndFeel.Default.SkinName
-            Call My.Settings.Default.Save()
+            Settings.Default("ApplicationSkinName") = UserLookAndFeel.Default.SkinName
+            Call Settings.Default.Save()
         End Sub
     End Class
 End Namespace
